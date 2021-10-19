f5d36aa6d7238ad3e30e6b4f6d9ad2eda2e6110a
br66190 <br66190@uga.edu>
2021-10-19 08:57:26 -0400

    insertion sort

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+
+const barDisplay = document.getElementById('display')
 function addBars(){
-    const slider = document.getElementById('myRange')
-    const bars = +slider.value;
-    for(let i =0; i < bars; i++){
-        
+    barDisplay.innerHTML=""
+    for(let i =0; i < 100; i++){
+        const size = Math.floor(Math.random()*100)
+        const ele = document.createElement('div')
+        ele.classList.add('bar')
+        ele.style.width = `${10}px`
+        ele.style.height = `${size}px`
+        document.getElementById('display').appendChild(ele)
     }
 }
-const form = document.getElementById('addBarForm')
-form.addEventListener('submit',addBars)
+
+function insertionSort(){
+    bars = document.getElementsByClassName('.bar')
+    for (let i = 1; i < bars.length; i++) {
+        let current = bars[i];
+        let j = i-1;
+        while ((j > -1) && (current.style.height < bars[j].style.height)) {
+            bars[j+1] = bars[j];
+            j--;
+            bars[j+1] = current;
+        }
+    }
+}
+barDisplay.addEventListener('click',addBars)
+document.getElementById('sort').addEventListener('click',insertionSort)
